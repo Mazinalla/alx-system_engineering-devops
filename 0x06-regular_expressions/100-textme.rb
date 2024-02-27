dbf6a486d6509bc9550295c474cc945b0a120f07
mazinalla <mazinelsadig8@gmail.com>
2024-02-28 02:37:37 +0300

    py

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,19 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/+1234567890,+9876543210,flags/).join
+
+def parse_log_entry(log_entry)
+    sender = log_entry.match(/\[from:([^\]]+)\]/)&.captures&.first
+    receiver = log_entry.match(/\[to:([^\]]+)\]/)&.captures&.first
+    flags = log_entry.match(/\[flags:([^\]]+)\]/)&.captures&.first
+  
+    "#{sender},#{receiver},#{flags}"
+  end
+  
+  if ARGV.length != 1
+    puts "Usage: #{$PROGRAM_NAME} 'log_entry'"
+    exit 1
+  end
+  
+  log_entry = ARGV[0]
+  result = parse_log_entry(log_entry)
+  puts result
+  
