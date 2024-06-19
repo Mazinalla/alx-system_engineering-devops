cb39bbb12db94908de55216d70b0d12940003c11
mazinalla <mazinelsadig8@gmail.com>
2024-06-19 05:51:00 +0300

    API-ad

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,45 +1,32 @@
 #!/usr/bin/python3
-"""
-Reddit API Utility Module
-
-This module provides a function to query the Reddit API and get the number of
-subscribers for a given subreddit. If an invalid subreddit is given, the 
-function will return 0.
-
-Functions:
-    number_of_subscribers(subreddit): Returns the number of subscribers for
-                                      the given subreddit.
-"""
-
+'''A module containing functions for working with the Reddit API.
+'''
 import requests
 
+
+BASE_URL = 'https://www.reddit.com'
+'''Reddit's base API URL.
+'''
+
+
 def number_of_subscribers(subreddit):
-    """
-    Query the Reddit API to get the number of subscribers for a given subreddit.
-    
-    Args:
-        subreddit (str): The name of the subreddit.
-        
-    Returns:
-        int: The number of subscribers. Returns 0 if the subreddit is invalid.
-    """
-    # Construct the URL for the subreddit's about page
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    # Set a custom User-Agent
-    headers = {'User-Agent': 'my-app/0.0.1'}
-    
-    try:
-        # Make a GET request to the URL
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        # Check the status code of the response
-        if response.status_code == 200:
-            # Parse the JSON data to get the number of subscribers
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            # If the status code is not 200, return 0 (invalid subreddit)
-            return 0
-    except requests.RequestException:
-        # Handle any potential exceptions and return 0
-        return 0
+    '''Retrieves the number of subscribers in a given subreddit.
+    '''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
+    }
+    res = requests.get(
+        '{}/r/{}/about/.json'.format(BASE_URL, subreddit),
+        headers=api_headers,
+        allow_redirects=False
+    )
+    if res.status_code == 200:
+        return res.json()['data']['subscribers']
+    return 0
