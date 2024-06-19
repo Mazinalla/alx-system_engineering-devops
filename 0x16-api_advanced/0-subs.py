4ce313bbf53a298e35269f61642009e7f68e7450
mazinalla <mazinelsadig8@gmail.com>
2024-06-19 07:16:26 +0300

    API-ad

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,21 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
+    """Returns the number of subscribers for a given subreddit."""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+    try:
+        response = requests.get(
+            f'https://www.reddit.com/r/{subreddit}/about.json',
+            headers={'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'}
+        )
+        # Check if the request was successful
+        if response.status_code == 200:
+            data = response.json()
+            subs = data.get("data", {}).get("subscribers", 0)
+            return subs
+        else:
+            return 0
+    except (requests.RequestException, ValueError, KeyError):
+        return 0
+
