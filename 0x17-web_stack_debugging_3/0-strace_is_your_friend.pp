b8120de11dc763acad2a11f52059f946950648d9
mazinalla <mazinelsadig8@gmail.com>
2024-06-21 03:20:50 +0300

    upgrade

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,21 +1,6 @@
-# Ensure the WordPress directory exists with the correct permissions and ownership
-file { '/var/www/html/wordpress':
-  ensure  => directory,
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0755',
-  recurse => true,
-  require => Package['apache2'], # Ensure apache2 is installed before managing the directory
-}
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-# Ensure Apache service is running
-service { 'apache2':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/var/www/html/wordpress'], # Restart Apache if the directory changes
-}
-
-# Ensure Apache package is installed
-package { 'apache2':
-  ensure => installed,
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
