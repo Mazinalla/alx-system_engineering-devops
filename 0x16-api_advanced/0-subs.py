64d68a624d95914fef3e96c9a0784ba03c972bef
mazinalla <mazinelsadig8@gmail.com>
2024-06-21 03:16:23 +0300

    upgrade

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""
+Contains the number_of_subscribers function
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    """Returns the number of subscribers for a given subreddit."""
+    if subreddit is None or type(subreddit) is not str:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    try:
+        response = requests.get(
+            f'https://www.reddit.com/r/{subreddit}/about.json',
+            headers={'User-Agent': (
+                '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'
+                )
+                }
+
+        )
+        # Check if the request was successful
+        if response.status_code == 200:
+            data = response.json()
+            subs = data.get("data", {}).get("subscribers", 0)
+            return subs
+        else:
+            return 0
+    except (requests.RequestException, ValueError, KeyError):
+        return 0
